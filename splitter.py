2a019f1928a3801251a0cbca137c6a0edcfdc0c8
Carson331 <59031657+Carson331@users.noreply.github.com>
2022-02-14 16:36:23 -0500

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -1,22 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 subTotal = 66
-personDict = {'Carson': 19, 'Sean': 14, 'Bijan': 9, 'Wray': 9, 'Ben': 15}
+personDict = {'Name1': 19, 'Name2': 14, 'Name3': 9, 'Name4': 9, 'Name5': 15}
 def percentPer(subTotal, personDict):
     res = []
     for person in personDict.values():
